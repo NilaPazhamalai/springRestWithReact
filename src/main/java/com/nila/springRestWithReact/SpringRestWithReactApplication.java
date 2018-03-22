f16d6deb397dc8e9e851585d3d2ab6fd683c454e
Nila Pazhamalai <28644384+NilaPazhamalai@users.noreply.github.com>
2018-03-22 09:46:26 +0000

    test spring

---
+++ src/main/java/com/nila/springRestWithReact/SpringRestWithReactApplication.java
@@ -0,0 +1,12 @@
+package com.nila.springRestWithReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWithReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWithReactApplication.class, args);
+	}
+}
